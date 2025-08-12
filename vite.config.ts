f8263749155a12c68b8bd18634ee7c287d48ab7a
gotop1711 <gotop1711@gmail.com>
2025-08-12 19:16:10 +0800

    fix building errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import dts from 'vite-plugin-dts'
+
+// Get dirname in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
